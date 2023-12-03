436968cd6725fc7ed1507cf1fda087e6dd867920
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2023-12-03 14:51:14 +1300

    Ep. 21

--- main.cpp
+++ main.cpp
@@ -52,14 +52,15 @@ int main(int argc, char* argv[]){
     SDL_Renderer* renderer = nullptr;
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface* surface = SDL_LoadBMP("./images/63b0dbdcdc45a0b.bmp"); // Donkey Kong
+    SDL_Surface* surface = SDL_LoadBMP("./images/water1.bmp"); // Donkey Kong
 
     // It is best to add this after loading the surface as you are shipping the data
     // to the GPU.
-    SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 0xFF, 0, 0XFF));
+    // SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 0xFF, 0, 0XFF));
 
 
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, surface);
 
     SDL_FreeSurface(surface);
     //screen = SDL_GetWindowSurface(window);
@@ -80,16 +81,28 @@ int main(int argc, char* argv[]){
 
     // Create a rectangle
     SDL_Rect rectangle;
-    rectangle.x = 50;
-    rectangle.y = 100;
-    rectangle.w = 200;
-    rectangle.h = 200;
+    rectangle.x = 0;
+    rectangle.y = 0;
+    rectangle.w = 640;
+    rectangle.h = 480;
 
     SDL_Rect rectangle2;
-    rectangle2.x = 50;
-    rectangle2.y = 100;
-    rectangle2.w = 200;
-    rectangle2.h = 200;
+    rectangle2.x = -639;
+    rectangle2.y = 0;
+    rectangle2.w = 640;
+    rectangle2.h = 480;
+
+    SDL_Rect rectangle3;
+    rectangle3.x = 0;
+    rectangle3.y = 0;
+    rectangle3.w = 640;
+    rectangle3.h = 480;
+
+    SDL_Rect rectangle4;
+    rectangle4.x = 0;
+    rectangle4.y = -480;
+    rectangle4.w = 640;
+    rectangle4.h = 480;
 
     bool gameIsRunning = true;
     while(gameIsRunning){
@@ -107,20 +120,20 @@ int main(int argc, char* argv[]){
             if(event.type == SDL_QUIT){
                 gameIsRunning = false;
             }
-            if(event.type == SDL_MOUSEMOTION){
-                rectangle2.x = event.motion.x;
-                rectangle2.y = event.motion.y;
-            }
+//            if(event.type == SDL_MOUSEMOTION){
+//                rectangle2.x = event.motion.x;
+//                rectangle2.y = event.motion.y;
+//            }
             if(event.type == SDL_MOUSEBUTTONDOWN){
                 if(event.button.button == SDL_BUTTON_LEFT){
-                    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_ADD);
+                    SDL_SetTextureBlendMode(texture2, SDL_BLENDMODE_NONE);
                 }
                 else if(event.button.button == SDL_BUTTON_RIGHT){
-                    SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_MOD);
+                    SDL_SetTextureBlendMode(texture2,SDL_BLENDMODE_BLEND);
                 }
             }
             else {
-                SDL_SetTextureBlendMode(texture,SDL_BLENDMODE_BLEND);
+                SDL_SetTextureBlendMode(texture2,SDL_BLENDMODE_MOD);
             }
 
 //          if(event.button.button == SDL_BUTTON_RIGHT){
@@ -132,14 +145,39 @@ int main(int argc, char* argv[]){
        SDL_SetRenderDrawColor(renderer, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
        SDL_RenderClear(renderer);
 
+       int w,h;
+       SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+       // Change this at some point, it is a hack to slow down the sim
+       SDL_Delay(20);
+
+       rectangle.x++;
+       if (rectangle.x > 639){
+           rectangle.x = -639;
+       }
+       rectangle2.x++;
+       if (rectangle2.x > 639){
+           rectangle2.x = -639;
+       }
+
+       rectangle3.y++;
+       if (rectangle3.y > 479){
+           rectangle3.y = -480;
+       }
+       rectangle4.y++;
+       if (rectangle4.y > 479){
+           rectangle4.y = -480;
+       }
        // Do our drawing
        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
        SDL_RenderDrawLine(renderer, 5, 5, 300, 300);
        
        // SDL_RenderDrawRect(renderer,&rectangle);
+       SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_NONE);
        SDL_RenderCopy(renderer, texture, NULL, &rectangle);
        SDL_RenderCopy(renderer, texture, NULL, &rectangle2);
 
+       SDL_RenderCopy(renderer, texture2, NULL, &rectangle3);
+       SDL_RenderCopy(renderer, texture2, NULL, &rectangle4);
        // Finally show what we've drawn
        SDL_RenderPresent(renderer);
 
